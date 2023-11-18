8ac3b1bdeed4fccc112e92f386767d8486a17c35
Igorsantos021 <santosigor868@gmail.com>
2023-11-17 22:24:42 -0300

    js

---
+++ Js/Js/app.js
@@ -0,0 +1,26 @@
+function escreveTexto(){
+    document.getElementById("titulo").innerText = "Curso de Javascrpt"
+    document.getElementById("texto").innerText = "Estou estudando javascript e espero aprender, ou pelo menos o guto espera que eu aprenda."
+}
+function escreveOi(mensagem){
+    document.write(mensagem)
+}
+
+function corDoFundo(cor){
+  document.body.style.backgroundColor=cor
+}
+
+function corDoFundo2(){
+    document.body.style.backgroundColor="orange"
+}
+
+function corDoFundo3(){
+    document.body.style.backgroundColor="red"
+}
+
+//  escreveTexto()
+//  corDoFundo("yellow")
+
+//  escreveOi("Teste1<br>")
+//  escreveOi("<strong>Mais um teste</strong>")
+//  escreveOi("<br>esse eh o ultimo")
